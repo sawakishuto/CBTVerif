043da8e3870741d160bf914055a31ce18a473fc6
Sawaki Shuto <swkshuto0208@icloud.com>
2024-12-19 14:36:36 +0900

    feat: 初期

---
+++ CoreBlueToothVerification/CoreBlueToothVerificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBlueToothVerificationApp.swift
+//  CoreBlueToothVerification
+//
+//  Created by 澤木柊斗 on 2024/12/18.
+//
+
+import SwiftUI
+
+@main
+struct CoreBlueToothVerificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CBTthVerificationView()
+        }
+    }
+}
