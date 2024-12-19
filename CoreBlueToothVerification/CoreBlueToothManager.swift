5eeac7c0201fc98a58f7a1e19165b4cca295bb0c
Sawaki Shuto <swkshuto0208@icloud.com>
2024-12-19 14:35:26 +0900

    centralManagarクラスを追加

--- CoreBlueToothVerification/CoreBlueToothManager.swift
+++ CoreBlueToothVerification/CoreBlueToothManager.swift
@@ -70,3 +70,77 @@ class PeripheralManager: NSObject, CBPeripheralManagerDelegate {
     }
 }
 
+class CentralManager: NSObject, CBCentralManagerDelegate {
+    var centralManager: CBCentralManager?
+    var centralPublisher = PassthroughSubject<String, Never>()
+
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            print("Central is powered on")
+            centralPublisher.send("Central is powered on")
+            startScanning()
+        } else {
+            print("Central state: \(central.state.rawValue)")
+        }
+    }
+
+    func startScanning() {
+        centralManager?.scanForPeripherals(withServices: [CBUUID(string: "180A")], options: nil)
+    }
+
+    func centralManager(
+        _ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+        advertisementData: [String: Any], rssi RSSI: NSNumber
+    ) {
+        let deviceName = peripheral.name ?? "Unknown Device"
+        print("Discovered peripheral: \(deviceName)")
+        centralPublisher.send("Discovered peripheral: \(deviceName)")
+    }
+
+    func peripheral(
+        _ peripheral: CBPeripheral, didDiscoverServices error: Error?
+    ) {
+        if let services = peripheral.services {
+            for service in services {
+                print("Discovered Service: \(service.uuid)")
+                centralPublisher.send("Discovered Service: \(service.uuid)")
+                if service.uuid == CBUUID(string: "180A") {
+                    peripheral.discoverCharacteristics([CBUUID(string: "180D")], for: service)
+
+                }
+            }
+        }
+    }
+
+    func peripheral(
+        _ peripheral: CBPeripheral,
+        didDiscoverCharacteristicsFor service: CBService, error: Error?
+    ) {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if characteristic.uuid == CBUUID(string: "180D") {
+                    print("Discovered Characteristic: \(characteristic.uuid)")
+                    centralPublisher.send("Discovered Characteristics")
+                    peripheral.readValue(for: characteristic)
+                }
+            }
+        }
+    }
+
+    func peripheral(
+        _ peripheral: CBPeripheral,
+        didUpdateValueFor characteristic: CBCharacteristic, error: Error?
+    ) {
+        centralPublisher.send("readValue")
+        if let value = characteristic.value {
+            let receivedString = String(data: value, encoding: .utf8) ?? "Invalid data"
+            print("Received from Peripheral: \(receivedString)")
+            centralPublisher.send(receivedString)
+        }
+    }
+}
