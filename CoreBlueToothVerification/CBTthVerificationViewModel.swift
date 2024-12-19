d86a47893e6d4dc6294ee7df48483795a86f59b3
Sawaki Shuto <swkshuto0208@icloud.com>
2024-12-19 14:36:19 +0900

    feat: VMはこっちでした

---
+++ CoreBlueToothVerification/CBTthVerificationViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  CBTthVerificationViewModel.swift
+//  CoreBlueToothVerification
+//
+//  Created by 澤木柊斗 on 2024/12/18.
+//
+
+import Foundation
+import Combine
+
+
+struct CBModel: Identifiable {
+    let id = UUID().uuidString
+    let dataString: String
+}
+
+class CBTthVerificationViewModel: ObservableObject {
+    let peripheralManager = PeripheralManager()
+    let centralManager = CentralManager()
+    var cancellable = [AnyCancellable]()
+    @Published var RecievedData = [CBModel]()
+
+    init() {
+        centralManager.centralPublisher.sink(receiveCompletion: { completion in
+            print("complete")
+        }, receiveValue: {
+            self.RecievedData.append(CBModel(dataString: $0))
+            }
+        )
+        .store(in: &cancellable)
+        peripheralManager.peripheralPublisher.sink(receiveCompletion: { completion in
+            print("complete")
+        }, receiveValue: {
+            self.RecievedData.append(CBModel(dataString: $0))
+            }
+        )
+        .store(in: &cancellable)
+    }
+
+
+}
