6a84053df2e01f095fa1ecf64d32dea828f2ac22
Sawaki Shuto <swkshuto0208@icloud.com>
2024-12-19 14:33:55 +0900

    add: 初期設定

---
+++ CoreBlueToothVerificationTests/CoreBlueToothVerificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBlueToothVerificationTests.swift
+//  CoreBlueToothVerificationTests
+//
+//  Created by 澤木柊斗 on 2024/12/18.
+//
+
+import Testing
+@testable import CoreBlueToothVerification
+
+struct CoreBlueToothVerificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
